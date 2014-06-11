e2b2b8da17235e280a1f32e7f17e2e8302cf2315
Aimee <AimeeKnight83@gmail.com>
2014-06-11 17:31:03 -0500

    set up full integration spec for creatings posts

--- spec/features/blogament/user_creates_new_post_spec.rb
+++ spec/features/blogament/user_creates_new_post_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 feature "User creates a post" do
-  let!(:post) { FactoryGirl.create(:post) }
 
-  scenario "views an index page of posts" do
-    user = User.new(email: 'aimee@gmail.com')
+  scenario "a logged in user created a post" do
+    Capybara.current_driver = Capybara.javascript_driver # :selenium by default
+    user = User.new(email: 'aimee@example.com', id: 1)
     set_current_user(user)
 
     visit blogament.root_path
     click_link "New Post"
+
     expect(page).to have_content("New post")
+    fill_in "Title", with: "Title Input"
+    page.execute_script('$("#tinymce4").tinymce().setContent("I am a little teapot.")')
+    #fill_in "Text", with: "I'm a little teapot"
+    attach_file "Image", 'spec/data/skating.jpg'
+    click_button "Create Post"
+    page.should have_content "Listing Posts"
+    current_path.should == posts_path
+    #page.should have_content "I'm a little teapot"
+    page.find('img')['src'].should have_content '/skating.jpg'
   end
 end
