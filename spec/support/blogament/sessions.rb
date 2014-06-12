72e5be71688499b1b51996c4b4f91cbc94f76a84
Aimee <AimeeKnight83@gmail.com>
2014-06-12 15:52:20 -0500

    add current user class  helper method to application controller to for views

--- spec/support/blogament/sessions.rb
+++ spec/support/blogament/sessions.rb
@@ -1,5 +1,6 @@
 def set_current_user(user)
   Blogament::ApplicationController.send(:define_method, :current_user) {}
+  Blogament::ApplicationController.send(:helper_method, :current_user) {}
 
   allow_any_instance_of(Blogament::ApplicationController).to(
     receive(:current_user).
