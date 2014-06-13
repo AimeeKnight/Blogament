4c3a1da0086c56d2878cbba5b8492f7733a344eb
Aimee <AimeeKnight83@gmail.com>
2014-06-13 10:21:24 -0500

    clean up styles and image uploader

--- app/uploaders/blogament/image_uploader.rb
+++ app/uploaders/blogament/image_uploader.rb
@@ -9,7 +9,7 @@ module Blogament
     # Choose what kind of storage to use for this uploader:
     storage :file
     # storage :fog
-    process :resize_to_fit => [800, 800]
+    process :resize_to_fit => [500, 500]
 
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
@@ -39,9 +39,9 @@ module Blogament
 
     # Add a white list of extensions which are allowed to be uploaded.
     # For images you might use something like this:
-    # def extension_white_list
-    #   %w(jpg jpeg gif png)
-    # end
+    def extension_white_list
+      %w(jpg jpeg gif png)
+    end
 
     # Override the filename of the uploaded files:
     # Avoid using model.id or version_name here, see uploader/store.rb for details.
