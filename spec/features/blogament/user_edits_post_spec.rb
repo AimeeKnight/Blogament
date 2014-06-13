6b1b088ac951956d16ab3b69143666db53971780
Aimee <AimeeKnight83@gmail.com>
2014-06-13 15:26:47 -0500

    edit travis.yml and add specs for editing a post that doesn't belong to the user

--- spec/features/blogament/user_edits_post_spec.rb
+++ spec/features/blogament/user_edits_post_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 feature "User edits a post" do
-
   scenario "a logged in user edits a post", :js do
     user = User.create!(email: 'aimee@example.com')
     set_current_user(user)
@@ -10,7 +9,6 @@ feature "User edits a post" do
     click_link "New Post"
     expect(page).to have_content("New post")
     fill_in 'Title', :with => 'Test Title'
-    #attach_file "Image", '/Users/aimeeknight/Desktop/NSS-Ruby/blogament/spec/data/skating.jpg'
     page.execute_script('$("#tinymce4").tinymce().setContent("Test Text from TinyMCE.")')
     click_button 'Create Post'
     expect(current_path).to eq blogament.posts_path
@@ -22,11 +20,21 @@ feature "User edits a post" do
     click_link "Edit"
     expect(page).to have_content("Edit")
     fill_in 'Title', :with => 'Test Title Edit'
-    #attach_file "Image", '/Users/aimeeknight/Desktop/NSS-Ruby/blogament/spec/data/skating.jpg'
+    attach_file "Image", '/Users/aimeeknight/Desktop/NSS-Ruby/blogament/spec/data/skating.jpg'
     page.execute_script('$("#tinymce4").tinymce().setContent("Test Edit Text from TinyMCE.")')
     click_button 'Update Post'
     expect(current_path).to eq blogament.posts_path
     expect(page).to have_content "Test Title Edit"
     expect(page).to have_content "Test Edit Text from TinyMCE."
+  end
+
+  scenario "a logged in user tries to edit a post they didn't create" do
+    user = User.create!(email: 'aimee@example.com')
+    create(:post)
+    set_current_user(user)
+
+    visit blogament.root_path
+    click_link "Full Post"
+    expect(page).to_not have_content("Edit")
   end
 end
