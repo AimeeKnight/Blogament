be53668e4198c2c5e5d9b07c5c097171e6d6b58f
Aimee <AimeeKnight83@gmail.com>
2014-06-23 12:22:25 -0500

    add feature specs for post search

---
+++ spec/features/blogament/user_searches_posts_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Searching posts" do
+  let!(:post) { FactoryGirl.create(:post) }
+
+  before(:each) do
+    Blogament::Post.reindex
+  end
+
+  scenario "as a logged in user" do
+    user = User.create!(email: 'aimee@example.com')
+    set_current_user(user)
+
+    visit blogament.root_path
+    fill_in "query", :with => "teapot"
+    click_button "Search"
+    expect(page).to have_content("Blogs")
+    expect(page).to have_content("Title")
+    expect(page).to have_content("Test Title")
+    expect(page).to have_content("I'm a little teapot")
+    expect(page).to have_content("New Post")
+  end
+
+  scenario "as a logged out user", mock_auth: { status: :unauthorized } do
+    user = User.create!(email: 'aimee@example.com')
+    set_current_user(nil)
+
+    visit blogament.root_path
+    fill_in "query", :with => "little"
+    click_button "Search"
+    expect(page).to have_content("Blogs")
+    expect(page).to have_content("Title")
+    expect(page).to have_content("Test Title")
+    expect(page).to have_content("I'm a little teapot")
+    expect(page).to_not have_content("New Post")
+  end
+end
