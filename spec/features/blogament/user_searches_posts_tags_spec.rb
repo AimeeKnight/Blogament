75706ce4cb02a1d3814f99a0929bf52216133ad9
Aimee <AimeeKnight83@gmail.com>
2014-06-23 15:31:59 -0500

    fix specs due to UI changes

--- spec/features/blogament/user_searches_posts_tags_spec.rb
+++ spec/features/blogament/user_searches_posts_tags_spec.rb
@@ -1,26 +1,26 @@
-require 'spec_helper'
+require "spec_helper"
 
 feature "Searching posts" do
   let!(:post) { FactoryGirl.create(:post) }
 
   scenario "as a logged in user" do
-    user = User.create!(email: 'aimee@example.com')
+    user = User.create!(email: "aimee@example.com")
     set_current_user(user)
 
     visit blogament.root_path
     click_link "New Post"
-    fill_in 'Title', :with => 'Test Title'
-    fill_in 'post[tag_list]', :with => 'JS'
-    click_button 'Create Post'
+    fill_in "Title", :with => "Test Title"
+    fill_in "post[tag_list]", :with => "JS"
+    click_button "Create Post"
     expect(current_path).to eq blogament.posts_path
     expect(page).to have_content "Test Title"
     expect(page).to have_content "JS"
 
     visit blogament.root_path
     click_link "New Post"
-    fill_in 'Title', :with => 'Test Title'
-    fill_in 'post[tag_list]', :with => 'Ruby'
-    click_button 'Create Post'
+    fill_in "Title", :with => "Test Title"
+    fill_in "post[tag_list]", :with => "Ruby"
+    click_button "Create Post"
     expect(current_path).to eq blogament.posts_path
     expect(page).to have_content "Test Title"
     expect(page).to have_content "Ruby"
@@ -28,20 +28,18 @@ feature "Searching posts" do
     visit blogament.root_path
     click_link "Ruby"
     expect(page).to have_content("Blogs")
-    expect(page).to have_content("Title")
     expect(page).to have_content("Test Title")
     expect(page).to have_content("Ruby")
     expect(page).to_not have_content("JS")
   end
 
   scenario "as a logged out user" do
-    user = User.create!(email: 'aimee@example.com')
+    user = User.create!(email: "aimee@example.com")
     set_current_user(nil)
 
     visit blogament.root_path
     #click_link "Ruby"
     expect(page).to have_content("Blogs")
-    expect(page).to have_content("Title")
     expect(page).to have_content("Test Title")
     expect(page).to have_content("I'm a little teapot")
     #expect(page).to have_content("Ruby")
